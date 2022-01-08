cb81116a17828dff9a6b1b5750c437e5e12da5a9
gjgustjd <gjgustjd70@naver.com>
2022-01-08 23:43:02 +0900

    viewBinding 적용

--- app/src/main/java/com/miso/weatherchat/MainActivity.kt
+++ app/src/main/java/com/miso/weatherchat/MainActivity.kt
@@ -2,10 +2,14 @@ package com.miso.weatherchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.miso.weatherchat.databinding.ActivityLoginBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
