a1a1eec2822daf1da27e7777ad2717e62d625622
gjgustjd <gjgustjd70@naver.com>
2022-01-09 17:36:03 +0900

    카카오 로그인 구현

--- app/src/main/java/com/miso/misoweather/MainActivity.kt
+++ app/src/main/java/com/miso/misoweather/MainActivity.kt
@@ -3,7 +3,9 @@ package com.miso.misoweather
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.kakao.sdk.auth.AuthApiClient
 import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.KakaoSdkError
 import com.kakao.sdk.user.UserApiClient
 import com.miso.misoweather.databinding.ActivityLoginBinding
 
@@ -15,21 +17,48 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.clBtnKakaoLogin.setOnClickListener {
-            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this@MainActivity)) {
-                UserApiClient.instance.loginWithKakaoTalk(this@MainActivity) { token, error ->
-                    if (error != null) {
-                        Log.e("miso", "로그인 실패", error)
-                    } else if (token != null) {
-                        Log.i("miso", "로그인 성공 ${token.accessToken}")
+          checkKakaoTokenAndLogin()
+        }
+    }
+
+    fun checkKakaoTokenAndLogin(){
+        if (AuthApiClient.instance.hasToken()) {
+            UserApiClient.instance.accessTokenInfo { _, error ->
+                if (error != null) {
+                    if (error is KakaoSdkError && error.isInvalidTokenError() == true) {
+                        //로그인 필요
+                        kakaoLogin()
+                    }
+                    else {
+                        //기타 에러
                     }
                 }
-            } else {
-                UserApiClient.instance.loginWithKakaoAccount(this@MainActivity) { token, error ->
-                    if (error != null) {
-                        Log.e("miso", "로그인 실패", error)
-                    } else if (token != null) {
-                        Log.i("miso", "로그인 성공 ${token.accessToken}")
-                    }
+                else {
+                    //토큰 유효성 체크 성공(필요 시 토큰 갱신됨)
+                }
+            }
+        }
+        else {
+            kakaoLogin()
+        }
+    }
+
+    fun kakaoLogin()
+    {
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(this@MainActivity)) {
+            UserApiClient.instance.loginWithKakaoTalk(this@MainActivity) { token, error ->
+                if (error != null) {
+                    Log.e("miso", "로그인 실패", error)
+                } else if (token != null) {
+                    Log.i("miso", "로그인 성공 ${token.accessToken}")
+                }
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(this@MainActivity) { token, error ->
+                if (error != null) {
+                    Log.e("miso", "로그인 실패", error)
+                } else if (token != null) {
+                    Log.i("miso", "로그인 성공 ${token.accessToken}")
                 }
             }
         }
