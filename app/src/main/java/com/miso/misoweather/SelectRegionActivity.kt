68f8cdc857650ef12aeaec3bd78541626f77b50c
gjgustjd <gjgustjd70@naver.com>
2022-01-15 16:36:55 +0900

    지역 목록 수정

--- app/src/main/java/com/miso/misoweather/SelectRegionActivity.kt
+++ app/src/main/java/com/miso/misoweather/SelectRegionActivity.kt
@@ -14,6 +14,7 @@ class SelectRegionActivity :AppCompatActivity(){
         super.onCreate(savedInstanceState);
         binding = ActivitySelectRegionBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initializeViews()
 
     }
     fun initializeViews()
