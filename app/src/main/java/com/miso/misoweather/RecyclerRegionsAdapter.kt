de0fb100a73dd5862236fd456caa33d99789ee0e
gjgustjd <gjgustjd70@naver.com>
2022-01-15 17:54:15 +0900

    버튼 선택 시 색상 변경 구현

--- app/src/main/java/com/miso/misoweather/RecyclerRegionsAdapter.kt
+++ app/src/main/java/com/miso/misoweather/RecyclerRegionsAdapter.kt
@@ -1,6 +1,7 @@
 package com.miso.misoweather
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,9 +9,12 @@ import android.widget.BaseAdapter
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import lombok.Getter
 
 class RecyclerRegionsAdapter(var context: Context, var regions:Array<String>):RecyclerView.Adapter<RecyclerRegionsAdapter.Holder>(){
 
+    var selectedPosition:Int=-1
+    var viewHolders:ArrayList<Holder> = ArrayList()
     
     override fun getItemCount(): Int {
         return regions.size
@@ -18,10 +22,23 @@ class RecyclerRegionsAdapter(var context: Context, var regions:Array<String>):Re
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         val layoutParams = holder.itemView.layoutParams
-        layoutParams.height=180
+        layoutParams.height=140
         holder.itemView.requestLayout()
         val data = regions.get(position)
        holder.setText(data)
+        holder.itemView.setOnClickListener{
+            if(selectedPosition!=-1) {
+                var previousSelectedViewHolder = viewHolders.get(selectedPosition)
+                previousSelectedViewHolder.itemView.setBackgroundResource(R.drawable.grid_region_background)
+                previousSelectedViewHolder.txt_name.setTextColor(Color.BLACK)
+            }
+
+            selectedPosition = position
+            viewHolders.get(selectedPosition).itemView.setBackgroundResource(R.drawable.grid_region_background_purple)
+            viewHolders.get(selectedPosition).txt_name.setTextColor(Color.WHITE)
+
+        }
+        viewHolders.add(holder)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder{
