14b1eca166295aac5e8c9aec96bd06ebc14f1ddd
gjgustjd <gjgustjd70@naver.com>
2022-01-16 02:07:45 +0900

    list_weather_on_time 추가

--- app/src/main/java/com/miso/misoweather/selectRegion/RecyclerTownsAdapter.kt
+++ app/src/main/java/com/miso/misoweather/selectRegion/RecyclerTownsAdapter.kt
@@ -2,7 +2,6 @@ package com.miso.misoweather.selectRegion
 
 import android.content.Context
 import android.graphics.Color
-import android.graphics.Paint
 import android.graphics.Typeface.BOLD
 import android.graphics.Typeface.NORMAL
 import android.view.LayoutInflater
