30b6674121b917c1e45843453432c83524423836
gjgustjd <gjgustjd70@naver.com>
2022-01-16 18:16:00 +0900

    풍속 및 습도 추가

---
+++ app/src/main/java/com/miso/misoweather/selectRegion/WeatherMainActivity.kt
@@ -0,0 +1,28 @@
+package com.miso.misoweather.selectRegion
+
+import android.os.Bundle
+import android.widget.LinearLayout.VERTICAL
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.miso.misoweather.R
+import com.miso.misoweather.common.VerticalSpaceItemDecoration
+import com.miso.misoweather.databinding.ActivitySelectRegionBinding
+import com.miso.misoweather.databinding.ActivityWeatherMainBinding
+
+class WeatherMainActivity :AppCompatActivity(){
+    lateinit var binding:ActivityWeatherMainBinding
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState);
+        binding = ActivityWeatherMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initializeViews()
+
+    }
+    fun initializeViews()
+    {
+    }
+
+}
