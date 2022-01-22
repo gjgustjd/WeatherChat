cdd689a26cb4869c30729a19415f0755d490adc4
gjgustjd <gjgustjd70@naver.com>
2022-01-23 01:46:51 +0900

    지역 선택 시 택일로 변경

--- app/src/main/java/com/miso/misoweather/selectRegion/RecyclerRegionsAdapter.kt
+++ app/src/main/java/com/miso/misoweather/selectRegion/RecyclerRegionsAdapter.kt
@@ -9,47 +9,44 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.miso.misoweather.R
 
-class RecyclerRegionsAdapter(var context: Context, var regions:Array<String>):RecyclerView.Adapter<RecyclerRegionsAdapter.Holder>(){
+class RecyclerRegionsAdapter(var context: Context, var regions: Array<String>) :
+    RecyclerView.Adapter<RecyclerRegionsAdapter.Holder>() {
 
-    var viewHolders:ArrayList<Holder> = ArrayList()
-    
+    var viewHolders: ArrayList<Holder> = ArrayList()
+    var selectedIndex: Int = -1
+
     override fun getItemCount(): Int {
         return regions.size
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         val layoutParams = holder.itemView.layoutParams
-        layoutParams.height=120
+        layoutParams.height = 120
         holder.itemView.requestLayout()
         val data = regions.get(position)
-       holder.setText(data)
-        holder.itemView.setOnClickListener{
-            if(holder.isSelected) {
-                holder.itemView.setBackgroundResource(R.drawable.grid_region_background)
-                holder.txt_name.setTextColor(context.resources.getColor(R.color.primaryPurple))
+        holder.setText(data)
+        holder.itemView.setOnClickListener {
+            if (selectedIndex != -1) {
+                var viewHolder = viewHolders.get(selectedIndex)
+                viewHolder.itemView.setBackgroundResource(R.drawable.grid_region_background)
+                viewHolder.txt_name.setTextColor(context.resources.getColor(R.color.primaryPurple))
             }
-            else
-            {
-                holder.itemView.setBackgroundResource(R.drawable.grid_region_background_purple)
-                holder.txt_name.setTextColor(Color.WHITE)
-            }
-            holder.isSelected=!holder.isSelected
-
+            selectedIndex = position
+            holder.itemView.setBackgroundResource(R.drawable.grid_region_background_purple)
+            holder.txt_name.setTextColor(Color.WHITE)
 
         }
         viewHolders.add(holder)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.grid_region,parent,false)
-            return Holder(view)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.grid_region, parent, false)
+        return Holder(view)
     }
 
-    class Holder(itemView:View):RecyclerView.ViewHolder(itemView){
+    class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var txt_name = itemView.findViewById<TextView>(R.id.txt_region_name)
-        var isSelected = false
-        fun setText(listData:String)
-        {
+        fun setText(listData: String) {
             txt_name.text = "${listData}"
         }
     }
