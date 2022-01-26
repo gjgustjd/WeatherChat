79aa397ca6c53b2eaac84d1bf7b0cffdc99ad1c0
gjgustjd <gjgustjd70@naver.com>
2022-01-26 22:40:59 +0900

    MisoWeather URL 고정 변수로 추가

--- app/src/main/java/com/miso/misoweather/common/MisoActivity.kt
+++ app/src/main/java/com/miso/misoweather/common/MisoActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.miso.misoweather.R
 
 open class MisoActivity :AppCompatActivity() {
+    val MISOWEATHER_BASE_URL:String = "http://3.35.55.100/"
     protected fun transferToBack(){
         overridePendingTransition(R.anim.slide_left_enter, R.anim.slide_left_exit)
     }
