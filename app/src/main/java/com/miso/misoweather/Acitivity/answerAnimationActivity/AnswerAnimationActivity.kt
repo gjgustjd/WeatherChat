372e8c9af7be66e951c4881d392a560a4aead587
gjgustjd <gjgustjd70@naver.com>
2022-02-12 22:01:01 +0900

    :heavy_plus_sign: Chore: Glide 의존성 추가
    Glide 의존성 추가

---
+++ app/src/main/java/com/miso/misoweather/Acitivity/answerAnimationActivity/AnswerAnimationActivity.kt
@@ -0,0 +1,41 @@
+package com.miso.misoweather.Acitivity.answerAnimationActivity
+
+import android.content.Intent
+import android.graphics.Paint
+import android.os.Bundle
+import android.util.Log
+import android.widget.*
+import com.bumptech.glide.Glide
+import com.miso.misoweather.common.MisoActivity
+import com.miso.misoweather.databinding.ActivitySelectNicknameBinding
+import com.miso.misoweather.Acitivity.home.HomeActivity
+import com.miso.misoweather.model.DTO.*
+import com.miso.misoweather.model.DTO.NicknameResponse.NicknameData
+import com.miso.misoweather.model.DTO.NicknameResponse.NicknameResponseDto
+import com.miso.misoweather.model.interfaces.MisoWeatherAPI
+import com.miso.misoweather.Acitivity.selectArea.SelectAreaActivity
+import com.miso.misoweather.databinding.ActivityAnimationMyanswerBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.Exception
+
+class AnswerAnimationActivity : MisoActivity() {
+    lateinit var binding: ActivityAnimationMyanswerBinding
+    lateinit var txt_answer:TextView
+    lateinit var img_animation:ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityAnimationMyanswerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initializeViews()
+
+    }
+
+    fun initializeViews() {
+        txt_answer = binding.txtAnswer
+        img_animation = binding.imgCheckAnimation
+    }
+}
