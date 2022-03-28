e5fbe4552fe0e457d40d53f7f15ca1977f5934b0
gjgustjd <gjgustjd70@naver.com>
2022-03-28 20:51:20 +0900

    🧪Test:CommonUtil.toIntString Test 추가
    toIntString 테스트 추가

--- app/src/test/java/com/miso/misoweather/common/CommonUtilTest.kt
+++ app/src/test/java/com/miso/misoweather/common/CommonUtilTest.kt
@@ -3,6 +3,7 @@ package com.miso.misoweather.common
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import com.google.common.truth.Truth.assertThat
 
 import org.junit.Assert.*
 
@@ -18,5 +19,9 @@ class CommonUtilTest {
 
     @Test
     fun toIntString() {
+        assertThat(CommonUtil.toIntString("1.00002202032")).isEqualTo("1")
+        assertThat(CommonUtil.toIntString("2.00002202032")).isEqualTo("2")
+        assertThat(CommonUtil.toIntString("")).isEqualTo("0")
+        assertThat(CommonUtil.toIntString("dddd")).isEqualTo("0")
     }
 }
