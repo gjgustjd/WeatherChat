0d4e323d67ca45054b7a6ae7dd56980a24a0986f
gjgustjd <gjgustjd70@naver.com>
2022-03-28 20:21:13 +0900

    🧪Test:CommonUtilTest 추가
    CommonUtilTest.kt 파일 추가

---
+++ app/src/test/java/com/miso/misoweather/common/CommonUtilTest.kt
@@ -0,0 +1,22 @@
+package com.miso.misoweather.common
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class CommonUtilTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun toIntString() {
+    }
+}
