62f36a3260fb4dcaafa0788a66a5219dd0b19c33
gjgustjd <gjgustjd70@naver.com>
2022-04-21 22:16:39 +0900

    :recycle:Refactor:SelectTownActivity 및 ViewModel에 DataStore 적용
    SelectTownActivity 및 ViewModel에 DataStore 적용

--- app/src/main/java/com/miso/misoweather/Acitivity/selectTown/SelectTownViewModel.kt
+++ app/src/main/java/com/miso/misoweather/Acitivity/selectTown/SelectTownViewModel.kt
@@ -3,31 +3,31 @@ package com.miso.misoweather.Acitivity.selectTown
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.miso.misoweather.Module.LiveDataModule.*
-import com.miso.misoweather.model.DTO.GeneralResponseDto
 import com.miso.misoweather.model.DTO.Region
-import com.miso.misoweather.model.DTO.RegionListResponse.RegionListResponseDto
+import com.miso.misoweather.model.DataStoreManager
 import com.miso.misoweather.model.MisoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.scopes.ActivityRetainedScoped
 import retrofit2.Response
 import java.lang.Exception
 import javax.inject.Inject
 
 @HiltViewModel
-class SelectTownViewModel @Inject constructor(private val repository: MisoRepository) : ViewModel() {
+class SelectTownViewModel @Inject constructor(private val repository: MisoRepository) :
+    ViewModel() {
 
-    @MutableNullableStringLiveData
-    @Inject
-    lateinit var smallScaleRegion: MutableLiveData<String?>
+    val midScaleRegion by lazy {
+        repository.dataStoreManager.getPreference(DataStoreManager.MIDSCALE_REGION)
+    }
 
-    @MutableNullableStringLiveData
-    @Inject
-    lateinit var midScaleRegion: MutableLiveData<String?>
+    val bigScaleRegion by lazy {
+        repository.dataStoreManager.getPreference(DataStoreManager.BIGSCALE_REGION)
+    }
 
-    @MutableNullableStringLiveData
-    @Inject
-    lateinit var bigScaleRegion: MutableLiveData<String?>
+    val misoToken by lazy {
+        repository.dataStoreManager.getPreference(DataStoreManager.MISO_TOKEN)
+    }
 
     @MutableResponseLiveData
     @Inject
@@ -39,53 +39,31 @@ class SelectTownViewModel @Inject constructor(private val repository: MisoReposi
 
 
     fun updateSmallScaleRegion(region: String) {
-        repository.apply {
-            addPreferencePair("SmallScaleRegion", region)
-            savePreferences()
-        }
-    }
-
-    fun setupSmallScaleRegion() {
-        smallScaleRegion.value = repository.getPreference("SmallScaleRegion")
-    }
-
-    fun setupMidScaleRegion() {
-        midScaleRegion.value = repository.getPreference("MidScaleRegion")
-    }
-
-    fun setupBigScaleRegion() {
-        bigScaleRegion.value = repository.getPreference("BigScaleRegion")
-    }
-
-    fun updateProperties() {
-        setupBigScaleRegion()
-        setupMidScaleRegion()
-        setupSmallScaleRegion()
+        repository.dataStoreManager.savePreference(DataStoreManager.SMALLSCALE_REGION, region)
     }
 
     fun addRegionPreferences(selectedRegion: Region) {
-        var midScaleRegion = selectedRegion.midScale
-        var bigScaleRegion = selectedRegion.bigScale
-        repository.apply {
-            addPreferencePair("BigScaleRegion", bigScaleRegion)
-            addPreferencePair("MidScaleRegion", midScaleRegion)
-            savePreferences()
+        val midScaleRegion = selectedRegion.midScale
+        val bigScaleRegion = selectedRegion.bigScale
+
+        repository.dataStoreManager.apply {
+            savePreference(DataStoreManager.BIGSCALE_REGION, bigScaleRegion)
+            savePreference(DataStoreManager.MIDSCALE_REGION, midScaleRegion)
         }
     }
 
     fun updateRegion(selectedRegion: Region, regionId: Int) {
         repository.updateRegion(
-            repository.getPreference("misoToken")!!,
+            misoToken,
             regionId,
             { call, response ->
                 try {
                     Log.i("changeRegion", "성공")
                     addRegionPreferences(selectedRegion)
-                    repository.addPreferencePair(
-                        "defaultRegionId",
+                    repository.dataStoreManager.savePreference(
+                        DataStoreManager.DEFAULT_REGION_ID,
                         regionId.toString()
                     )
-                    repository.savePreferences()
                     updateRegionResponse.value = response
                 } catch (e: Exception) {
                     e.printStackTrace()
