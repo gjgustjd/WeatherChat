9ded36cee03b03bb72137157c0496fc10db5c977
gjgustjd <gjgustjd70@naver.com>
2022-04-24 17:15:44 +0900

    :art:Feat:RecyclerAdapterModule 추가
    RecyclerAdapter용 Module 추가

---
+++ app/src/main/java/com/miso/misoweather/Module/RecyclerAdapterModule.kt
@@ -0,0 +1,53 @@
+package com.miso.misoweather.Module
+
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.GridLayoutManager
+import com.miso.misoweather.Acitivity.selectRegion.RecyclerRegionsAdapter
+import com.miso.misoweather.Acitivity.selectRegion.RegionItem
+import com.miso.misoweather.R
+import com.miso.misoweather.common.VerticalSpaceItemDecoration
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.android.scopes.ActivityScoped
+
+@Module
+@InstallIn(ActivityComponent::class)
+class RecyclerAdapterModule {
+    @RequiresApi(Build.VERSION_CODES.M)
+    @Provides
+    @ActivityScoped
+    fun getRegionAdapter(@ActivityContext context: Context): RecyclerRegionsAdapter {
+        fun getRegionItems(): ArrayList<RegionItem> {
+            val resource = context.resources
+            val regions = resource.getStringArray(R.array.regions)
+            val regions_full = resource.getStringArray(R.array.regions_full)
+            val regionItems = arrayListOf<RegionItem>()
+            for (i: Int in regions.indices) {
+                val item = RegionItem().apply {
+                    shortName = regions[i]
+                    longName = regions_full[i]
+                }
+                regionItems.add(item)
+            }
+            return regionItems
+        }
+        return RecyclerRegionsAdapter(context, getRegionItems())
+    }
+
+    @Provides
+    @ActivityScoped
+    fun getGridLayoutManager(@ActivityContext context: Context): GridLayoutManager {
+        return GridLayoutManager(context, 4)
+    }
+
+    @Provides
+    fun getVerticalSpaceItemDecoration(): VerticalSpaceItemDecoration {
+        return VerticalSpaceItemDecoration(30)
+    }
+
+}
