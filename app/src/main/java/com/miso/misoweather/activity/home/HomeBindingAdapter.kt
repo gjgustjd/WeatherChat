08878bd6408a42f63d110ad3d892fd8ede168d7c
gjgustjd <gjgustjd70@naver.com>
2022-04-25 23:04:59 +0900

    :art:Refactor:RecyclerChatsAdapter에 DataBinding 적용
    RecyclerChatsAdapter에 DataBinding 적용

--- app/src/main/java/com/miso/misoweather/activity/home/HomeBindingAdapter.kt
+++ app/src/main/java/com/miso/misoweather/activity/home/HomeBindingAdapter.kt
@@ -4,10 +4,15 @@ import android.os.Build
 import android.view.View
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.miso.misoweather.R
 import com.miso.misoweather.model.dto.commentList.Comment
 import com.miso.misoweather.model.dto.surveyResultResponse.SurveyResultResponseDto
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 object HomeBindingAdapter {
     @BindingAdapter("emptyLayoutVisibility")
@@ -40,7 +45,6 @@ object HomeBindingAdapter {
         }
     }
 
-
     @BindingAdapter("answer")
     @JvmStatic
     fun setAnswer(view: TextView, surveyResultResponseDto: SurveyResultResponseDto?) {
@@ -63,13 +67,69 @@ object HomeBindingAdapter {
         }
     }
 
-
     @RequiresApi(Build.VERSION_CODES.O)
     @BindingAdapter("bindData")
     @JvmStatic
     fun setRecycerData(view: RecyclerView, commentList: List<Comment>?) {
         commentList?.let { it ->
             view.adapter = RecyclerChatsAdapter(commentList, false)
+        }
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @BindingAdapter("commentNickname")
+    @JvmStatic
+    fun setCommentNickName(view: TextView, comment: Comment) {
+        view.text = "${comment.bigScale}의 ${comment.nickname}"
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @BindingAdapter("commentContent")
+    @JvmStatic
+    fun setCommentContent(view: TextView, comment: Comment) {
+        view.text = comment.content
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @BindingAdapter("commentEmoji")
+    @JvmStatic
+    fun setCommentEmoji(view: TextView, comment: Comment) {
+        view.text = comment.emoji
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @BindingAdapter("commentBackground")
+    @JvmStatic
+    fun setCommentBackground(view: ConstraintLayout, isCommentFragment: Boolean) {
+        if (isCommentFragment)
+            view.setBackgroundResource(R.drawable.unit_background)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @BindingAdapter("commentTime")
+    @JvmStatic
+    fun setCommentTime(view: TextView, comment: Comment) {
+        val currentTimeString =
+            ZonedDateTime.now(ZoneId.of("Asia/Seoul"))
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd")).toString()
+
+        val commentCreatedAt = comment.createdAt
+        if (commentCreatedAt != null) {
+            if (commentCreatedAt.contains("T")) {
+                val commentDayString =
+                    commentCreatedAt.split("T")[0]
+                if (commentDayString.equals(currentTimeString))
+                    view.text =
+                        commentCreatedAt.split("T")[1].split(".")[0].substring(0, 5)
+                else
+                    view.text = commentDayString + " " +
+                            commentCreatedAt.split("T")[1].split(".")[0].substring(0, 5)
+            } else {
+                view.text = commentCreatedAt
+            }
+        } else {
+            view.text = ""
         }
     }
 }
--- app/src/main/java/com/miso/misoweather/activity/home/RecyclerChatsAdapter.kt
+++ app/src/main/java/com/miso/misoweather/activity/home/RecyclerChatsAdapter.kt
@@ -1,20 +1,14 @@
 package com.miso.misoweather.activity.home
 
 import android.os.Build
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
-import com.miso.misoweather.activity.chatmain.ChatMainViewModel
 import com.miso.misoweather.R
 import com.miso.misoweather.databinding.ListItemChatBinding
 import com.miso.misoweather.model.dto.commentList.Comment
-import java.time.ZoneId
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
-import javax.inject.Inject
 
 @RequiresApi(Build.VERSION_CODES.O)
 class RecyclerChatsAdapter(
@@ -22,10 +16,6 @@ class RecyclerChatsAdapter(
     private val isCommentsFragment: Boolean,
 ) :
     RecyclerView.Adapter<RecyclerChatsAdapter.Holder>() {
-
-    @Inject
-    lateinit var viewModel: ChatMainViewModel
-    private var viewHolders: ArrayList<Holder> = ArrayList()
     val currentBindedPosition = MutableLiveData<Int>()
 
     override fun getItemCount(): Int {
@@ -42,40 +32,8 @@ class RecyclerChatsAdapter(
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         currentBindedPosition.value = position
-        holder.nickname.text =
-            "${comments.get(position).bigScale}의 ${comments.get(position).nickname}"
-        holder.comment.text = comments.get(position).content
 
-        val currentTimeString =
-            ZonedDateTime.now(ZoneId.of("Asia/Seoul"))
-                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd")).toString()
-
-        val commentCreatedAt = comments.get(position).createdAt
-        if (commentCreatedAt != null) {
-            if (commentCreatedAt.contains("T")) {
-                val commentDayString =
-                    commentCreatedAt.split("T")[0]
-                if (commentDayString.equals(currentTimeString))
-                    holder.time.text =
-                        commentCreatedAt.split("T")[1].split(".")[0].substring(0, 5)
-                else
-                    holder.time.text = commentDayString + " " +
-                            commentCreatedAt.split("T")[1].split(".")[0].substring(0, 5)
-            } else {
-                holder.time.text = commentCreatedAt
-            }
-        } else {
-            holder.time.text = ""
-        }
-
-        holder.emoji.text = comments.get(position).emoji
-
-        if (isCommentsFragment)
-            holder.background.setBackgroundResource(R.drawable.unit_background)
-
-        viewHolders.add(holder)
-        Log.i("onBindViewHolder", position.toString())
-
+        holder.bind(comments[position])
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
@@ -84,12 +42,11 @@ class RecyclerChatsAdapter(
         return Holder(ListItemChatBinding.bind(view))
     }
 
-    inner class Holder(binding: ListItemChatBinding) : RecyclerView.ViewHolder(binding.root) {
-        var background = binding.background
-        var nickname = binding.txtNickname
-        var comment = binding.txtMessage
-        var time = binding.txtTime
-        var emoji = binding.txtEmoji
+    inner class Holder(val binding: ListItemChatBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(comment: Comment) {
+            binding.comment = comment
+            binding.isCommentFragment = isCommentsFragment
+        }
     }
 
 }
